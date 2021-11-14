459708841ef97ad1653e9acb817538774432db77
sandilya761 <57587519+sandilya761@users.noreply.github.com>
2021-11-14 20:15:20 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
+#%%
+import math
 from Crypto.Cipher import AES
-from base64 import b64encode
-from base64 import b64decode
 from Crypto.Util.Padding import pad, unpad
 
-block = 16
+from bitstring import BitStream, BitArray
 
+block = 16
+#%%
 # AEScipher class
 class AEScipher:
     def __init__(self, key, plain_text):  
@@ -23,52 +25,150 @@ class AEScipher:
           # Setup cipher
         original_data = cipher.decrypt(unpad(cipher_text,block))
         return original_data
+#%%
+radix = 10
+minlen = 6
+maxlen = 56
+#T = "ABD212A5D4C842"  # tweak string of length 56 (14 pairs of hexadecimal)
+n = 16 # length of ciphertext
+#X =  "9234525091124167"  # plaintext
+#K = b'1234507891423245'
+
+T = "ABD212A5D4C842"  # tweak string of length 56 (14 pairs of hexadecimal)
+X =  "1200448754451312"  # plaintext
+K = b'73@4019@29807965'
 
 
 
-def byte_xor(ba1, ba2):
-    """ XOR two byte strings """
-    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
-
-# Function for veryfing the format
-def format(cipher):
-    for i in cipher:
-        if i>=48 and i<=57:
-            pass
-        else:
-            return False
-    return True
-
-# global variables
-key = b'123495678abcde5$'# 16 bytes which is 16*8 = 128
-plain_text = b'492a947532202293' # 16 bytes
-
-def feistel_forward(key_,plain_text_):
-    l=[]
-    r=[]
-    aes = []
-    l.append(plain_text_[0:8])
-    r.append(plain_text_[8:16])
-
-    for j in range(7):
-        aes.append(AEScipher(key_,r[j]))
-        tmp = aes[j].encrypt()
-        r.append(byte_xor(tmp,l[j]))
-        l.append(r[j])
-    return l,r
-l,r = feistel_forward(key,plain_text)        
-
-count = 0
-x=10
-while(x):
-    l,r = feistel_forward(key,l[7]+r[7])        
-    count = count+1
-    x= x-1
-
-print(count)
-print(l[7]+r[7])
-
-    
-    
+u = math.ceil(n/2) # u = 8
+v = n-u
+A = X[0:8] # first 8 digits of plaintext(x)
+B = X[8:16]
 
 
+# %%
+
+T_l = T[0:8]
+T_r = T[8:16] + T[7] + str(0)  
+
+#%%
+
+# Functions
+
+def num_radix(X):
+    x = 0
+    for i in range(1,len(X)):# length of plaintext = 16
+        x = x*radix + int(X[i])
+    return int(x)
+
+def string_radix(x,m): # v=8
+    #m = len(x)
+    X = [0 for i in range(0,m)]
+    x = int(x)
+    for i in range(0,m):
+        X[m-1-i] = x % radix
+        x = math.floor(x/radix) # floor value
+    k = ""
+    for i in range(m):    
+        k = k + str(X[i])
+    return k# returns the plaintext
+  
+
+def num(X):
+    x = 0
+    for i in range(0,len(X)):
+        x = 2*x + int(X[i])
+    return int(x)
+
+          
+def reverse(X):
+    Y=X
+    Y=list(Y)
+    X=list(X)
+    k = ""
+    for i in range(0,len(X)):
+        Y[i] = X[len(X)-1-i]
+        k = k + Y[i]
+    return k
+
+def reverse_bytes(X):
+    bytes_size = math.ceil(len(X)/8)
+    r = [[] for i in range(bytes_size)]    
+    count = 0
+    for i in range(len(X)):
+        if i % 8 == 0 and i != 0:
+            count = count + 1        
+        r[count].append(X[i])
+    a = ""
+    for i in range(len(r)):
+        for j in range(8):
+            a = a + r[len(r)-1-i][j]
+    return a
+
+def xor(a, b):
+    y = int(a,2) ^ int(b,2)
+    return '{0:b}'.format(y)
+
+# %%
+# good tested method to convert bits and bytes in python 
+# would be to access the stack and play with raw bits but it's out of scope
+# we use the bitstring to do bidding for us
+# converting bits to bytes example:
+# BitArray('0b1011').tobytes()
+# converting bytes to bits example:
+# a = BitArray(b'\x00\xd2')
+# using a.bin to get the bit string
+# Encryption
+aes = []
+for i in range(0,8):
+    if i%2==0:
+        m = u # u = 8
+        W = T_r
+    else:
+        m = v # v = 8
+        W = T_l
+    # converting hex to bin
+    P = xor(bin(int(W,16))[2:], ('{:032b}'.format(i))) + '{:0192b}'.format((num_radix(reverse(str(B)))))    
+    aes.append(AEScipher(BitArray('0b'+reverse_bytes(BitArray(K).bin)).tobytes(),BitArray('0b'+reverse_bytes(P)).tobytes()))
+    CIPH = aes[i].encrypt()
+    S = reverse_bytes(BitArray(CIPH).bin)
+    y = num(S)
+    c = (num_radix(reverse(A))+y) % (radix**m)
+    C = reverse(string_radix(c,m))
+    A = B
+    B = C
+print(A+B)
+# %%
+Y = A+B
+A = Y[0:8] # first 8 digits of plaintext(x)
+B = Y[8:16]
+
+aes = []
+for j in range(8):
+    i = 7 - j
+    if i%2==0:
+        m = u # u = 8
+        W = T_r
+    else:
+        m = v # v = 8
+        W = T_l
+    # converting hex to bin
+    P = xor(bin(int(W,16))[2:], ('{:032b}'.format(i))) + '{:0192b}'.format((num_radix(reverse(str(A)))))    
+    aes.append(AEScipher(BitArray('0b'+reverse_bytes(BitArray(K).bin)).tobytes(),BitArray('0b'+reverse_bytes(P)).tobytes()))
+    CIPH = aes[j].encrypt()
+    S = reverse_bytes(BitArray(CIPH).bin)
+    y = num(S)
+    c = (num_radix(reverse(B))-y) % (radix**m)
+    C = reverse(string_radix(c,m))
+    B = A
+    A = C
+print(A+B)
+print(len(A+B))
+
+print(X == A+B)
+print(A)
+print(X[0:8])
+
+
+
+# %%
